1db46eb315869c96ac4aa77c27cfd58e96ccfc8e
Lidiia Deich <lidiiardeich@gmail.com>
2023-01-01 16:30:08 -0700

    add comuterMove script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == '1')
+  computerMove = 'kamień';
+  
+elseif(randomNumber == '2')
+	computerMove= 'papier';
+ 
+elseif(randomNumber == '3')
+	computerMove = 'nożyce';  
+
+
+printMessage('Mój ruch to: ' + computerMove);
+
 
 function printMessage('Zagrałem' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
 {
