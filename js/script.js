985a9180fac7858eb5a400efbe7c549f7b25ac65
Lidiia Deich <lidiiardeich@gmail.com>
2023-01-01 14:56:52 -0700

    add index.html

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let computerMove = 'kamień';
+let playerMove = 'papier';
+
+function printMessage('Zagrałem' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+{
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages()
+{
+	document.getElementById('messages').innerHTML = '';
+}
